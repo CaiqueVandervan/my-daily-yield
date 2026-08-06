2fb153f644876ae21490a2f8e1f0d84d0dc99f1a
CaiqueVandervan <caiquevja481@gmail.com>
2026-08-06 17:24:40 -0300

    feat: add NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+export default function () {
+    return (
+        <nav className="bg-[#151D33]/95 border-b border-amber-200/20">
+            <div className="grid grid-cols-3 text-white p-10 px-20">
+                <div className="justify-self-start">
+                    logo
+                </div>
+                <div className="justify-self-center">
+                    options
+                </div>
+                <div className="justify-self-end">
+                    aditional informations
+                </div>
+            </div>
+        </nav>
+    )
+}
