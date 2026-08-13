025aa3fa99c328a95d3777ca662d1c1a50e922c5
CaiqueVandervan <caiquevja481@gmail.com>
2026-08-13 17:49:45 -0300

    feat: add shadcn components and Its dependencies

---
+++ src/components/cn/DatePicker.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import * as React from "react"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { cn } from "@/utils/cn"
+import { Button } from "@/components/cn/Button"
+import { Calendar } from "@/components/cn/Calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/cn/Popover"
+
+export function DatePicker() {
+    const [date, setDate] = React.useState<Date>()
+
+    return (
+        <Popover>
+            <PopoverTrigger
+                render={
+                    <Button
+                        variant="outline"
+                        data-empty={!date}
+                        className="justify-start text-left font-normal data-[empty=true]:text-muted-foreground"
+                    />
+                }
+            >
+                <CalendarIcon />
+                {date ? format(date, "PPP") : <span>Pick a date</span>}
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+                <Calendar mode="single" selected={date} onSelect={setDate} />
+            </PopoverContent>
+        </Popover>
+    )
+}
