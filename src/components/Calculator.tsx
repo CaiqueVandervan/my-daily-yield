88c207b54128d5f5a9032fa66dfa02cc2d358062
CaiqueVandervan <caiquevja481@gmail.com>
2026-08-14 15:56:15 -0300

    feat: starting Calculator tab structure

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,8 +1,15 @@
 export default function Calculator() {
 
     return (
-        <section className="bg-[#151D33] flex-1 m-3 p-4 rounded-xl border border-amber-200/20 text-red-400">
-            Calculator
+        <section className="grid grid-cols-3 bg-[rgb(21,29,51)] flex-1 m-3 rounded-xl border border-amber-200/20 text-red-400">
+
+            <div className="grid justify-center p-4">
+                <div>
+                    <h1 className="text-[50px] text-white">Calculator</h1>
+                    <p className="text-zinc-400 text-[18px]">Compound interest calculator</p>
+                </div>
+            </div>
+
         </section>
     )
 }
